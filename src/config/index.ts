546a2494b37af2deb5bf833d4c7089d974e024ce
ScriptKev <script.kev@gmail.com>
2021-04-03 15:56:00 -0400

    Actualizando URI de produccion

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,12 +9,12 @@ type TConfig = {
 }
 
 const uri: string = 'mongodb+srv://kevma:kevmaDB123@kevma.qg2z2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-const uriDev = 'mongodb://localhost/kevma-db'
+// const uriDev = 'mongodb://localhost/kevma-db'
 
 const config: TConfig = {
   PORT: process.env.PORT || 3500,
   DB: {
-    URI: uriDev
+    URI: uri
   }
 }
 
